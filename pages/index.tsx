43454ceb0288268f99e0c71bb0226bafbad3be52
ds-okubo <t-okubo@digitalstage.jp>
2021-04-15 15:30:56 +0900

    head・title・body 要素追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,39 +15,46 @@ const Home = () => {
   };
 
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Demo</h1>
-      <form
-        className={styles.search}
-        onSubmit={e => {
-          e.preventDefault();
-          output();
-        }}
-      >
-        <input
-          type='text'
-          placeholder={`🔍  Search for photos`}
-          value={query}
-          onChange={e => setQuery(e.target.value)}
-        />
-      </form>
-      <div className={styles.list}>
-        {photos.map((photo, index) => {
-          return (
-            <div key={index}>
-              <a href={photo.regular} target='_blank' rel='noopener noreferrer'>
-                <img src={photo.small} />
-              </a>
-              <div>
-                <a href={photo.profileurl} target='_blank' rel='noopener noreferrer'>
-                  📸 {photo.author}
-                </a>
-              </div>
-            </div>
-          )
-        })}
-      </div>
-    </div>
+    <>
+      <head>
+        <title>Demo</title>
+      </head>
+      <body>
+        <div className={styles.container}>
+          <h1 className={styles.title}>Demo</h1>
+          <form
+            className={styles.search}
+            onSubmit={e => {
+              e.preventDefault();
+              output();
+            }}
+          >
+            <input
+              type='text'
+              placeholder={`🔍  Search for photos`}
+              value={query}
+              onChange={e => setQuery(e.target.value)}
+            />
+          </form>
+          <div className={styles.list}>
+            {photos.map((photo, index) => {
+              return (
+                <div key={index}>
+                  <a href={photo.regular} target='_blank' rel='noopener noreferrer'>
+                    <img src={photo.small} />
+                  </a>
+                  <div>
+                    <a href={photo.profileurl} target='_blank' rel='noopener noreferrer'>
+                      📸 {photo.author}
+                    </a>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </body>
+    </>
   )
 };
 
